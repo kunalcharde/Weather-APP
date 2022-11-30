05c2776ee65fea73d2122a9ca43f56d1b3583e35
Kunal Charde <kunalcharde1@gmail.com>
2022-12-01 01:18:44 +0530

    All updated Code

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
-var loca = document.getElementById("location");
-var key = document.getElementById("access_key");
-const url = `http://api.weatherstack.com/current?access_key=${key.value}&query=${loca.value}`;
+function accesskey() {
+  const inputLocation = document.getElementById("CityName");
+  const inputkey = document.getElementById("access_key");
 
-function accesskey(){
-    checkforerror(loca.value,key.value)
-}
+  if (inputLocation.value !== "" && inputkey.value !== "") {
+    const loc = document.getElementById("CityName").value;
+    const key = document.getElementById("access_key").value;
+    const url = `http://api.weatherstack.com/current?access_key=${key}&query=${loc}}`;
 
-function checkforerror(input1,input2){
-    if(input1 ===""){
-        alert("Please Enter Location")
+    async function wheatherdata(url) {
+      const response = await fetch(url);
+      data = await response.json();
+      console.log(data);
+      console.log(data.address);
+      document.getElementById("Location").innerHTML = "Location : " + data.location.name;
+      document.getElementById("lat").innerHTML = "Lat : " + data.location.lat;
+      document.getElementById("Time_Zone").innerHTML =
+        "Time Zone : " + data.location.timezone_id;
+      document.getElementById("Wind_speed").innerHTML =
+        "Wind Speed : " + data.current.wind_speed;
+      document.getElementById("pressure").innerHTML =
+        "Pressure : " + data.current.pressure;
+      document.getElementById("Humidity").innerHTML =
+        "Humidity : " + data.current.humidity;
+      document.getElementById("Wind_Direction").innerHTML =
+        "Wind Direction :  " + data.current.wind_dir;
+      document.getElementById("UV_Index").innerHTML =
+        "UV Index : " + data.current.uv_index;
+      document.getElementById("Feels_like").innerHTML =
+        "Feels like : " + data.current.feelslike;
     }
-    else if (input2 ===""){
-        alert("Please Enter Accesskey")
-    }
-    else{
-        wheatherdata();
-    }
+    wheatherdata(url);
+  } else if (inputLocation.value == "") {
+    alert("Please Enter Valid Location");
+  } else if (acesskey.value == "") {
+    alert("Please Enter Valid Acess Token");
+  }
 }
-
-function wheatherdata(){
-    
-    fetch(url).then(data => data.json()).then(responseData => {
-        document.getElementById("Location").innerHTML= "Location : "+responseData.location.name;
-        document.getElementById("lat").innerHTML= "Lat : "+responseData.location.lat;
-        document.getElementById("Time_Zone").innerHTML= "Time Zone : "+responseData.location.timezone_id;
-        document.getElementById("Wind_speed").innerHTML= "Wind Speed : "+responseData.current.wind_speed;
-        document.getElementById("Humidity").innerHTML= "Humidity : "+responseData.current.humidity;
-        document.getElementById("Wind_Direction").innerHTML= "Wind Direction :  "+responseData.current.wind_dir;
-        document.getElementById("UV_Index").innerHTML= "UV Index : "+responseData.current.uv_index;
-        document.getElementById("Feels_like").innerHTML= "Feels like : "+responseData.current.feelslike;
-    })
-
-}
-
